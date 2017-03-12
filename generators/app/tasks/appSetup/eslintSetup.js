cea5b100d4976757bf4f077bc467998123b41bf0
Sebastian Balay <snbalay@gmail.com>
2017-03-12 20:09:33 -0300

    move all eslint setup to its own file

--- generators/app/tasks/appSetup/eslintSetup.js
+++ generators/app/tasks/appSetup/eslintSetup.js
@@ -1,3 +1,9 @@
 module.exports = function eslintSetup() {
+  const packageJson = this.fs.readJSON(this.destinationPath(this.projectName, 'package.json'));
+  packageJson.scripts = packageJson.scripts || {};
+  packageJson.scripts.lint = 'eslint .';
+  packageJson.scripts['lint-fix'] = 'eslint . --fix';
+  this.fs.writeJSON(this.destinationPath(this.projectName, 'package.json'), packageJson);
+
   this.fs.copy(this.templatePath('.eslintrc.ejs'), this.destinationPath(this.projectName, '.eslintrc.js'));
 };
--- generators/app/tasks/appSetup/packageJsonScripts.js
+++ generators/app/tasks/appSetup/packageJsonScripts.js
@@ -1,8 +1,6 @@
 module.exports = function packgeJsonScripts() {
   const packageJson = this.fs.readJSON(this.destinationPath(this.projectName, 'package.json'));
   packageJson.scripts = packageJson.scripts || {};
-  packageJson.scripts.lint = 'eslint .';
-  packageJson.scripts['lint-fix'] = 'eslint . --fix';
   packageJson.scripts.clean = 'rm -rf $TMPDIR/react-* && watchman watch-del-all && npm cache clean';
   packageJson.scripts['clean:android'] = 'cd android/ && ./gradlew clean';
   packageJson.scripts[
