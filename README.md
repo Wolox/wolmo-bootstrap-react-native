54ee49d2dc29d42ca1b127371682ff474fcefdb8
Sebastian Balay <snbalay@gmail.com>
2017-03-13 16:10:15 -0300

    Fix bash script to run the bootstrap

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 ## Important
 You don't need to clone this repository. Just run the following in your terminal:
 ```bash
-curl -o- https://raw.githubusercontent.com/sbalay/react-native-bootstrap/master/run.sh | bash
+bash <(curl -s https://raw.githubusercontent.com/sbalay/react-native-bootstrap/master/run.sh)
 ```
--- run.sh
+++ run.sh
@@ -3,25 +3,30 @@
 
 { # this ensures the entire script is downloaded #
 
+echo "Getting initial dependencies..."
+
 system_has() {
   type "$1" > /dev/null 2>&1
 }
 
 if ! system_has git; then
-  echo "\033[1m\033[31mgit is mandatory to continue \033[0m"
-  echo "\033[1m\033[31mCheck this guide to complete the installation: https://git-scm.com/book/en/v2/Getting-Started-Installing-Git \033[0m"
+  echo "git is mandatory to continue"
+  echo "Check this guide to complete the installation: https://git-scm.com/book/en/v2/Getting-Started-Installing-Git"
 elif ! system_has node; then
-  echo "\033[1m\033[31mInstalling node \033[0m"
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
-  sh "$HOME/.nvm/nvm.sh"
-  nvm install 7
-  nvm use 7
+  echo "Installing node"
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash > /dev/null 2>&1
+  sh "$HOME/.nvm/nvm.sh" > /dev/null 2>&1
+  nvm install 7 > /dev/null 2>&1
+  nvm use 7 > /dev/null 2>&1
 elif ! system_has yarn; then
-  curl -o- -L https://yarnpkg.com/install.sh | bash
+  echo "Installing yarn"
+  curl -o- -L https://yarnpkg.com/install.sh | bash > /dev/null 2>&1
   export PATH="$HOME/.yarn/bin:$PATH"
 fi
 
-yarn global add yeoman wolmo-bootstrap-rn
+yarn global add yeoman generator-wolmo-bootstrap-rn > /dev/null 2>&1
 yo wolmo-bootstrap-rn
+
+wait $!
 
 } # this ensures the entire script is downloaded #
