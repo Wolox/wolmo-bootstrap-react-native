e7d10fae08e81346e0a4833ec9a28f14514f8036
Sebastian Balay <snbalay@gmail.com>
2017-04-21 12:13:40 -0300

    improve android scripts

--- generators/app/tasks/appSetup/packageJsonScripts.js
+++ generators/app/tasks/appSetup/packageJsonScripts.js
@@ -1,13 +1,13 @@
 module.exports = function packgeJsonScripts() {
   const packageJson = this.fs.readJSON(this.destinationPath(this.projectName, 'package.json'));
   packageJson.scripts = packageJson.scripts || {};
-  packageJson.scripts.clean = 'rm -rf $TMPDIR/react-* && watchman watch-del-all && npm cache clean';
-  packageJson.scripts['clean:android'] = 'cd android/ && ./gradlew clean';
-  packageJson.scripts['force-clean'] =
-    'rm -rf $TMPDIR/react-* && watchman watch-del-all && rm -rf ios/build && rm -rf node_modules/ && npm cache clean && npm i';
   packageJson.scripts.coverage = 'jest --coverage';
-  packageJson.scripts['android:build'] = 'cd android && ./gradlew assembleRelease';
+  packageJson.scripts.clean = 'rm -rf $TMPDIR/react-* && watchman watch-del-all && npm cache clean';
+  packageJson.scripts['force-clean'] =
+    'npm run android:clean && rm -rf $TMPDIR/react-* && watchman watch-del-all && rm -rf ios/build && rm -rf node_modules/ && npm cache clean && npm i';
+  packageJson.scripts['android:clean'] = 'cd android/ && ./gradlew clean';
+  packageJson.scripts['android:build'] = 'cd android && ./gradlew clean && ./gradlew assembleRelease';
   packageJson.scripts['android:install'] =
-    'cd android && ./gradlew assembleRelease && ./gradlew installRelease';
+    'cd android && ./gradlew clean && ./gradlew assembleRelease && ./gradlew installRelease';
   this.fs.writeJSON(this.destinationPath(this.projectName, 'package.json'), packageJson);
 };
