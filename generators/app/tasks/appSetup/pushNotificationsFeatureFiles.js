3ea7e248c55b67ce973d1a909b28c041c549df98
Sebastian Balay <snbalay@gmail.com>
2017-08-02 13:03:15 -0300

    use abstracted code to copy files and template of push notifications

--- generators/app/tasks/appSetup/pushNotificationsFeatureFiles.js
+++ generators/app/tasks/appSetup/pushNotificationsFeatureFiles.js
@@ -1,27 +1,23 @@
+const { copyFile } = require('../../utils');
+
+const REDUX_PATH = 'src/redux';
+const CONFIG_PATH = 'src/config';
+const SERVICES_PATH = 'src/services';
+
+const PUSH_NOTIFICATIONS_REDUCER = `${REDUX_PATH}/pushNotifications/reducer.js`;
+const PUSH_NOTIFICATIONS_ACTIONS = `${REDUX_PATH}/pushNotifications/actions.js`;
+const PUSH_NOTIFICATIONS_SERVICE = `${SERVICES_PATH}/PushNotificationsService.js`;
+const PUSH_NOTIFICATIONS_CONFIG = `${CONFIG_PATH}/PushNotifications.js`;
+const HUAWEI_DEVICES_CONFIG = `${CONFIG_PATH}/HuaweiPushNotifications.js`;
+
+const FILES = [
+  PUSH_NOTIFICATIONS_REDUCER,
+  PUSH_NOTIFICATIONS_ACTIONS,
+  PUSH_NOTIFICATIONS_SERVICE,
+  PUSH_NOTIFICATIONS_CONFIG,
+  HUAWEI_DEVICES_CONFIG
+];
+
 module.exports = function pushNotificationsFeatureFiles() {
-  // src/redux/pushNotifications/reducer.js
-  this.fs.copy(
-    this.templatePath('src', 'redux', 'pushNotifications', 'reducer.js'),
-    this.destinationPath(this.projectName, 'src', 'redux', 'pushNotifications', 'reducer.js')
-  );
-  // src/redux/pushNotifications/actions.js
-  this.fs.copy(
-    this.templatePath('src', 'redux', 'pushNotifications', 'actions.js'),
-    this.destinationPath(this.projectName, 'src', 'redux', 'pushNotifications', 'actions.js')
-  );
-  // src/services/PushNotificationsService.js
-  this.fs.copy(
-    this.templatePath('src', 'services', 'PushNotificationsService.js'),
-    this.destinationPath(this.projectName, 'src', 'services', 'PushNotificationsService.js')
-  );
-  // src/config/PushNotifications.js
-  this.fs.copy(
-    this.templatePath('src', 'config', 'PushNotifications.js'),
-    this.destinationPath(this.projectName, 'src', 'config', 'PushNotifications.js')
-  );
-  // src/config/HuaweiPushNotifications.js
-  this.fs.copy(
-    this.templatePath('src', 'config', 'HuaweiPushNotifications.js'),
-    this.destinationPath(this.projectName, 'src', 'config', 'HuaweiPushNotifications.js')
-  );
+  FILES.forEach(copyFile.bind(this));
 };
