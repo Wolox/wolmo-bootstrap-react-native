c921966150cc9446cd10e8bd5799be71c11b2d2f
Ignacio Abadie <ignacio.abadie@wolox.com.ar>
2017-08-08 15:02:25 -0300

    Added Custom Text component to base proyect

---
+++ generators/app/templates/src/app/components/CustomText/index.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { Text } from 'react-native';
+
+import styles from './styles';
+
+export default class CustomText extends PureComponent {
+  static VARIANTS = [
+    'xxsmall',
+    'xsmall',
+    'small',
+    'medium',
+    'big',
+    'xbig',
+    'bold',
+    'center',
+    'white',
+    'blue',
+    'green',
+    'gray'
+  ];
+
+  customStyles = () =>
+    CustomText.VARIANTS
+      .map(variant => (this.props[variant] ? styles[variant] : null))
+      .filter(style => style !== null);
+
+  render() {
+    return (
+      <Text {...this.props.textProps} style={[styles.base, this.customStyles(), this.props.style]}>
+        {this.props.children}
+      </Text>
+    );
+  }
+}
+
+CustomText.defaultProps = {
+  textProps: {}
+};
+
+CustomText.propTypes = {
+  children: PropTypes.node.isRequired,
+  textProps: PropTypes.shape({ ...Text.propTypes })
+};
+
+CustomText.VARIANTS.forEach(variant => {
+  CustomText.propTypes[variant] = PropTypes.bool;
+});
---
+++ generators/app/templates/src/app/components/CustomText/styles.js
@@ -0,0 +1,51 @@
+import { StyleSheet } from 'react-native';
+
+import { blue, white, green, gray, black, transparent } from '../../../utils/colors';
+import { IS_SMALL_DEVICE } from '../../../utils/constants';
+
+const amountToResize = 2 * IS_SMALL_DEVICE;
+
+export default StyleSheet.create({
+  base: {
+    fontFamily: 'poppins',
+    fontSize: 16 - amountToResize,
+    backgroundColor: transparent,
+    color: black
+  },
+  gray: {
+    color: gray
+  },
+  white: {
+    color: white
+  },
+  blue: {
+    color: blue
+  },
+  green: {
+    color: green
+  },
+  xxsmall: {
+    fontSize: 7 - amountToResize
+  },
+  xsmall: {
+    fontSize: 11 - amountToResize
+  },
+  small: {
+    fontSize: 13 - amountToResize
+  },
+  medium: {
+    fontSize: 14 - amountToResize
+  },
+  big: {
+    fontSize: 20 - amountToResize
+  },
+  xbig: {
+    fontSize: 37 - amountToResize
+  },
+  bold: {
+    fontWeight: 'bold'
+  },
+  center: {
+    textAlign: 'center'
+  }
+});
