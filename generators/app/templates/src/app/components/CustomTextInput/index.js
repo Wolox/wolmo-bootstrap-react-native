f7684654ddbbc7afa3889be4ac9e8bc7a372419f
Adrian Ponce <adrian.ponce@wolox.com.ar>
2018-03-01 19:39:07 -0600

    Add Title Animation to TextInput

--- generators/app/templates/src/app/components/CustomTextInput/index.js
+++ generators/app/templates/src/app/components/CustomTextInput/index.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { View, TextInput, Text } from 'react-native';
+import { Animated, View, TextInput, Text, TouchableWithoutFeedback } from 'react-native';
 import PropTypes from 'prop-types';
 
 import CustomText from '../CustomText';
@@ -12,36 +12,125 @@ import styles from './styles';
 // https://github.com/facebook/react-native/issues/5859
 
 class CustomTextInput extends PureComponent {
-  state = { showPassword: false };
+  state = {
+    showPassword: false,
+    focused: false,
+    value: this.props.value,
+    focusedAnim: new Animated.Value(this.props.value ? 1 : 0)
+  };
 
   handleShowPassword = () => {
     this.setState(prevState => ({ showPassword: !prevState.showPassword }));
   };
 
-  render() {
-    const placeholderTextColor = this.props.value ? transparent : this.props.placeholderTextColor;
+  toggle = isActive => {
+    const { animationDuration, easing, useNativeDriver } = this.props;
+    this.isActive = isActive;
+    Animated.timing(this.state.focusedAnim, {
+      toValue: isActive ? 1 : 0,
+      duration: animationDuration,
+      easing,
+      useNativeDriver
+    }).start();
+  };
 
+  handleChange = value => {
+    this.setState({
+      value
+    });
+
+    if (this.props.onChangeText) {
+      this.props.onChangeText(value);
+    }
+  };
+
+  handleBlur = event => {
+    if (!this.state.value) {
+      this.toggle(false);
+    }
+    this.setState({ focused: false });
+
+    if (this.props.onBlur) {
+      this.props.onBlur(event);
+    }
+  };
+
+  handleFocus = event => {
+    this.toggle(true);
+    this.setState({ focused: true });
+
+    if (this.props.onFocus) {
+      this.props.onFocus(event);
+    }
+  };
+
+  focus = () => {
+    this.textInput.focus();
+  };
+
+  render() {
+    const { focusedAnim, focused, value } = this.state;
+    const { placeholderTextColor } = this.props;
     return (
       <View>
-        {this.props.title && (
-          <CustomText gray small style={[styles.title, this.props.titleStyles]}>
-            {this.props.title}
-          </CustomText>
-        )}
+        {this.props.title &&
+          !this.props.animated && (
+            <CustomText
+              gray
+              small
+              style={[styles.title, this.props.titleStyles, focused && this.props.titleFocusedStyles]}
+            >
+              {this.props.title}
+            </CustomText>
+          )}
         <View
           style={[
             this.props.multiline ? styles.multilineContainer : styles.container,
             this.props.bottomBorder && styles.bottomBorder,
-            this.props.style
+            this.props.style,
+            focused && this.props.focusedStyle
           ]}
         >
+          {this.props.title &&
+            this.props.animated && (
+              <TouchableWithoutFeedback onPress={this.focus}>
+                <Animated.View
+                  style={{
+                    position: 'absolute',
+                    bottom: focusedAnim.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [this.props.titleYUnfocus, this.props.titleYFocus]
+                    })
+                  }}
+                >
+                  <Animated.Text
+                    style={[
+                      styles.title,
+                      this.props.titleStyles,
+                      focused && this.props.titleFocusedStyles,
+                      {
+                        fontSize: focusedAnim.interpolate({
+                          inputRange: [0, 1],
+                          outputRange: [this.props.titleSizeUnfocus, this.props.titleSizeFocus]
+                        })
+                      }
+                    ]}
+                  >
+                    {this.props.title}
+                  </Animated.Text>
+                </Animated.View>
+              </TouchableWithoutFeedback>
+            )}
           <TextInput
             {...this.props}
+            ref={textInput => {
+              this.textInput = textInput;
+            }}
             allowFontScaling={false}
-            onChangeText={this.props.onChange}
-            onBlur={this.props.onBlur}
-            onFocus={this.props.onFocus}
-            value={this.props.value}
+            onChangeText={this.handleChange}
+            onBlur={this.handleBlur}
+            onFocus={this.handleFocus}
+            value={value}
             style={[
               styles.inputStyle,
               this.props.multiline ? styles.multilineInput : styles.singleInput,
@@ -74,7 +163,13 @@ CustomTextInput.defaultProps = {
   keyboardType: 'default',
   multiline: false,
   bottomBorder: false,
-  maxHeight: 200
+  maxHeight: 200,
+  animationDuration: 300,
+  animated: false,
+  titleYUnfocus: 16,
+  titleYFocus: 26,
+  titleSizeUnfocus: 14,
+  titleSizeFocus: 12
 };
 
 CustomTextInput.propTypes = {
@@ -89,15 +184,25 @@ CustomTextInput.propTypes = {
   multiline: PropTypes.bool,
   placeholder: PropTypes.string,
   titleStyles: Text.propTypes.style,
+  titleFocusedStyles: Text.propTypes.style,
   textStyles: Text.propTypes.style,
   onBlur: PropTypes.func,
-  onChange: PropTypes.func,
+  onChangeText: PropTypes.func,
   onFocus: PropTypes.func,
   value: PropTypes.string,
   placeholderTextColor: PropTypes.string,
   showEye: PropTypes.bool,
   secureTextEntry: PropTypes.bool,
-  title: PropTypes.string
+  title: PropTypes.string,
+  easing: PropTypes.func,
+  animationDuration: PropTypes.number,
+  useNativeDriver: PropTypes.bool,
+  animated: PropTypes.bool,
+  titleYUnfocus: PropTypes.number,
+  titleYFocus: PropTypes.number,
+  titleSizeUnfocus: PropTypes.number,
+  titleSizeFocus: PropTypes.number,
+  focusedStyle: View.propTypes.style
 };
 
 export default CustomTextInput;
