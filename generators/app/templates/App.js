9fb7a70dc3316c83924fbb49aba7ca32fc9a9e84
Anisospina <anisospina@gmail.com>
2019-08-25 14:12:05 -0500

    removed line break in index.ejs file

--- generators/app/templates/App.js
+++ generators/app/templates/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import '@config';
 import store from '@redux/store';
+
 import App from '@app';
 
 export default function index() {
