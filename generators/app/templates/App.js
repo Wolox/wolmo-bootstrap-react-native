80a4ec4f1c0ef099a57220ca639f2f8314882bed
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2019-08-29 00:36:39 -0300

    Wolmo fixes after bad merge and refactor

--- generators/app/templates/App.js
+++ generators/app/templates/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from '@redux/store';
+import React from "react";
+import { Provider } from "react-redux";
+import "@config";
+import store from "@redux/store";
 
-import App from '@app';
-import '@config';
+import App from "@app";
 
 export default function index() {
   return (
