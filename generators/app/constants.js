49376baba249d233b396eea74d7df96247a94409
David Tito <tdavid2000@hotmail.com>
2019-09-25 22:29:38 -0300

    removed crashlytics comments

--- generators/app/constants.js
+++ generators/app/constants.js
@@ -1,8 +1,2 @@
-module.exports.CRASHLYTICS_LINKS = {
-  ANDROID_INSTALL: 'https://invertase.io/oss/react-native-firebase/v6/crashlytics/android',
-  IOS_INSTALL: 'https://invertase.io/oss/react-native-firebase/v6/crashlytics/ios',
-  RN_INSTALL: 'https://invertase.io/oss/react-native-firebase/v6/crashlytics/quick-start'
-};
-
 module.exports.PUSH_NOTIFICATIONS_SETUP_LINK =
   'https://facebook.github.io/react-native/docs/pushnotificationios.html';
