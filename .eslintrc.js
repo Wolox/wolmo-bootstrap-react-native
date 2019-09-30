cc28fb79a6ac58c91ff2dcb7477708bdc25e04be
guillewolox <guillermo.sequeira@wolox.com.ar>
2019-09-30 14:53:38 -0300

    delete rules in .esLint project

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,6 @@ module.exports = {
   extends: ['wolox-react'],
   rules: {
     'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'no-magic-numbers': "off",
-    'react/no-multi-comp': 'off',
-    'eslint no-shadow:': 'off',
-    'init-declarations': 'off',
-    "no-empty-function": "off",
-    'complexity': 'off',
-    'no-extra-parens': 'off',
-    'new-cap': 'off',
-    'no-shadow': 'off',
-    'react/forbid-foreign-prop-types': 'off',
-    'prefer-promise-reject-errors': 'off',
-    'camelcase': 'off',
-    "react/jsx-closing-bracket-location": 'off'
+    'import/no-extraneous-dependencies': 'off'
   }
 };
