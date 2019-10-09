4a3d671b7b56ee469d3f866c7a09b612aeb901b4
Felipe Rodriguez <frodriguez@Woloxs-Mac-mini.local>
2019-10-09 10:37:47 -0300

    modify auth tests

--- generators/app/templates/__tests__/redux/auth/actions.js
+++ generators/app/templates/__tests__/redux/auth/actions.js
@@ -1,47 +1,33 @@
-import { actionCreators, actions } from '@redux/auth/actions';
+import {actionCreators, actions} from '@redux/auth/actions';
 import api from '@config/api';
 import AsyncStorage from '@react-native-community/async-storage';
 
-import { store } from '../store';
+import {store} from '../store';
+import {mapActionsToTypes} from '../utils';
 
 describe('testing auth actions', () => {
   beforeEach(() => {
     store.clearActions();
   });
   test('test AUTH_INIT', async () => {
-    const expectedActions = [{ type: actions.AUTH_INIT, target: 'currentUser', payload: {} }];
+    const expectedActions = [actions.AUTH_INIT];
     await store.dispatch(actionCreators.init({}));
-    expect(store.getActions()).toEqual(expectedActions);
+    expect(mapActionsToTypes(store.getActions())).toEqual(expectedActions);
   });
   test('test LOGIN SUCCESS AND INJECTIONS', async () => {
     api.setHeader = jest.fn();
     AsyncStorage.setItem = jest.fn().mockReturnValue(null);
-    const expectedActions = [
-      { type: actions.LOGIN, target: 'currentUser' },
-      { type: actions.LOGIN_SUCCESS, target: 'currentUser', payload: { sessionToken: 'token' } },
-      {
-        preserveFocus: true,
-        routeName: 'App',
-        type: 'Navigation/JUMP_TO',
-      }
-    ];
+    const expectedActions = [actions.LOGIN, actions.LOGIN_SUCCESS, 'Navigation/JUMP_TO'];
     await store.dispatch(actionCreators.login({}));
-    expect(store.getActions()).toEqual(expectedActions);
+    expect(mapActionsToTypes(store.getActions())).toEqual(expectedActions);
     expect(api.setHeader.mock.calls).toHaveLength(1);
     expect(AsyncStorage.setItem.mock.calls).toHaveLength(1);
   });
   test('test LOGOUT', async () => {
     AsyncStorage.removeItem = jest.fn().mockReturnValue(null);
-    const expectedActions = [
-      { type: actions.LOGOUT, target: 'currentUser' },
-      {
-        preserveFocus: true,
-        routeName: 'Auth',
-        type: 'Navigation/JUMP_TO',
-      }
-    ];
+    const expectedActions = [actions.LOGOUT, 'Navigation/JUMP_TO'];
     await store.dispatch(actionCreators.logout({}));
-    expect(store.getActions()).toEqual(expectedActions);
+    expect(mapActionsToTypes(store.getActions())).toEqual(expectedActions);
     expect(AsyncStorage.removeItem.mock.calls).toHaveLength(1);
   });
 });
