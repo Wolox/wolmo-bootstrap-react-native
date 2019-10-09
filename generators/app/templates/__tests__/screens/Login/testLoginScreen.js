770edf7697af87d7fb436b263dd15c486f9dae48
Felipe Rodriguez <frodriguez@Woloxs-Mac-mini.local>
2019-10-09 09:50:34 -0300

    remove shallow

--- generators/app/templates/__tests__/screens/Login/testLoginScreen.js
+++ generators/app/templates/__tests__/screens/Login/testLoginScreen.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import CustomTextInput from '@components/CustomTextInput';
 import CustomButton from '@components/CustomButton';
 import { Login } from '@app/screens/Login/layout';
