ffd4ad6dd38b154b8fdd48433cdda73618f2f9df
Felipe Rodriguez <frodriguez@Woloxs-Mac-mini.local>
2019-10-09 09:41:19 -0300

    change snapshot library

--- generators/app/templates/__tests__/screens/Login/testLoginScreen.js
+++ generators/app/templates/__tests__/screens/Login/testLoginScreen.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import CustomTextInput from '@components/CustomTextInput';
 import CustomButton from '@components/CustomButton';
 import { Login } from '@app/screens/Login/layout';
+import renderer from 'react-test-renderer';
 
 describe('<Login />', () => {
   test('without errors', () => {
@@ -13,7 +14,7 @@ describe('<Login />', () => {
   });
   test('Login Snapshot', () => {
     const onLogin = jest.fn();
-    const wrapper = shallow(<Login onLogin={onLogin} />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const tree = renderer.create(<Login onLogin={onLogin} />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
