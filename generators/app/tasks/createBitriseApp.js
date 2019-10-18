344896fd3c1db0c4f83f8c4d55ee152269790508
Felipe Rodriguez <frodriguez@Woloxs-Mac-mini.local>
2019-10-18 14:34:53 -0300

    fix

--- generators/app/tasks/createBitriseApp.js
+++ generators/app/tasks/createBitriseApp.js
@@ -7,7 +7,7 @@ const gitApi = {
     github: githubApi,
     gitlab: gitlabApi,
     bitbucket: bitbucketApi
-}
+};
 
 const createApp = async ({repoUrl, isPublic, gitOwner, provider, repoSlug, type}) => {
     return await bitriseApi.post('/apps/register', {
@@ -18,7 +18,7 @@ const createApp = async ({repoUrl, isPublic, gitOwner, provider, repoSlug, type}
         git_repo_slug: repoSlug,
         type: type
       })
-}
+};
 
 const createSshOnBitbucket = async (repoSlug, publicSshKey) => {
     const userResponse = await gitApi.bitbucket.get('/user/keys');
@@ -28,7 +28,8 @@ const createSshOnBitbucket = async (repoSlug, publicSshKey) => {
         key: publicSshKey
     })
     
-}
+};
+
 const createSshGit = async ({repoSlug, publicSshKey, provider}) => { // IMPORTANT -> GITLAB AND GITHUB HAVE THE SAME EP
     if(provider === 'bitbucket') 
         return await createSshOnBitbucket(repoSlug, publicSshKey);
@@ -37,7 +38,7 @@ const createSshGit = async ({repoSlug, publicSshKey, provider}) => { // IMPORTAN
             title: `bitrise-${repoSlug}`,
             key: publicSshKey
         })
-}
+};
 
 const registerSshKeyOnBitrise = async ({slug, publicSshKey, privateSshKey}) => {
     return await bitriseApi.post(`/apps/${slug}/register-ssh-key`, {
@@ -45,7 +46,7 @@ const registerSshKeyOnBitrise = async ({slug, publicSshKey, privateSshKey}) => {
         auth_ssh_public_key: publicSshKey,
         is_register_key_into_provider_service: false
       })
-}
+};
 
 const finishBitrise = async ({slug}) => {
     return await bitriseApi.post(`/apps/${slug}/finish`, {
@@ -55,16 +56,17 @@ const finishBitrise = async ({slug}) => {
         project_type: "react-native",
         stack_id: "osx-xcode-10.2.x"
       })
-}
+};
+
 const loadYmlToBitrise = async ({slug, bitriseYml}) => {
     return await bitriseApi.post(`/apps/${slug}/bitrise.yml`, {
         app_config_datastore_yaml: bitriseYml
       })
-}
+};
 
 const loadWebHook = async ({slug}) => {
     return await bitriseApi.post(`/apps/${slug}/register-webhook`);
-}
+};
 
 const setAuthenticationHeaders = ({gitToken, bitriseToken, provider}) => {
     bitriseApi.setHeaders({
@@ -73,7 +75,7 @@ const setAuthenticationHeaders = ({gitToken, bitriseToken, provider}) => {
     gitApi[provider].setHeaders({
         Authorization: gitToken
     });
-}
+};
 
 module.exports = async function createBitriseApp(values){
     setAuthenticationHeaders(values);
@@ -88,4 +90,4 @@ module.exports = async function createBitriseApp(values){
     await finishBitrise(newValues);
     await loadYmlToBitrise(newValues);
     await loadWebHook(newValues);
-}
+};
