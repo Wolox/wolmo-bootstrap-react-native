e64fe41375d57072681bb13ce8bef9d0be12a7b5
Matias Grote <matiasgrote@woloxs-MacBook-Pro.local>
2020-02-17 16:47:28 -0300

    Fix

--- generators/app/tasks/configureFastlane.js
+++ generators/app/tasks/configureFastlane.js
@@ -1,7 +1,8 @@
 const runCommand = require('./runCommand');
 
 const fastlaneMobileHTTPS = 'https://github.com/Wolox/fastlane-mobile.git';
-const fastlaneMvCommand = 'fastlane-mobile/fastlane ios/fastlane';
+const fastlaneFolderOrigin = 'fastlane-mobile/fastlane';
+const fastlaneFolderDestination = 'fastlane';
 const removeFastlaneMobie = 'fastlane-mobile';
 
 module.exports = function configureFastlane() {
@@ -18,7 +19,12 @@ module.exports = function configureFastlane() {
   })
     .then(() =>
       runCommand({
-        command: ['mv', [`${fastlaneMvCommand}`], { cwd: `${process.cwd()}/${this.projectName}/ios` }],
+        command: [
+          'mv',
+          [`${fastlaneFolderOrigin}`, `${fastlaneFolderDestination}`],
+          { cwd: `${process.cwd()}/${this.projectName}/ios` }
+        ],
+        successMessage: 'Move Success!',
         context: this.options
       })
     )
@@ -29,7 +35,25 @@ module.exports = function configureFastlane() {
           ['-rf', `${removeFastlaneMobie}`],
           { cwd: `${process.cwd()}/${this.projectName}/ios` }
         ],
+        successMessage: 'Delete Success!',
         context: this.options
       })
-    );
+    )
+    .then(() => {
+      const fastlaneFastfile = this.fs.read(`${this.projectName}/ios/fastlane/Fastfile`);
+      const updateFastlaneFastfile = fastlaneFastfile.replace(
+        "import 'Fastfile.private'",
+        `import 'Fastfile.private'\n
+desc "Update Bundle Identifier"
+lane :update_bundle_identifier do |options|
+  project_name = options[:project_name]
+  update_app_identifier(
+    xcodeproj: "./#{project_name}.xcodeproj", # Optional path to xcodeproj, will use the first .xcodeproj if not set
+    plist_path: "./#{project_name}/Info.plist", # Path to info plist file, relative to xcodeproj
+    app_identifier: "#{options[:bundle_identifier]}" # The App Identifier
+  )
+end`
+      );
+      this.fs.write(`${this.projectName}/ios/fastlane/Fastfile`, updateFastlaneFastfile);
+    });
 };
