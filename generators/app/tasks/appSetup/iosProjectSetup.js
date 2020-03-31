8118461bc3456e17afbcfd86b31d0eb600f92f2a
Felipe Rodriguez Esturo <felipe.rodriguez@wolox.com.ar>
2020-03-31 18:50:20 -0300

    multiple envs config for ios

--- generators/app/tasks/appSetup/iosProjectSetup.js
+++ generators/app/tasks/appSetup/iosProjectSetup.js
@@ -1,11 +1,17 @@
+const createSchemes = require("./multipleEnvIos/createSchemes");
+
 module.exports = function fixBundleIndentifier() {
   const iosProjectContent = this.fs.read(
     `${this.projectName}/ios/${this.projectName}.xcodeproj/project.pbxproj`
   );
-  const replaceRegex = new RegExp(`PRODUCT_NAME = ${this.projectName};`, 'g');
+  const replaceRegex = new RegExp(`PRODUCT_NAME = ${this.projectName};`, "g");
   const fixedProjectContent = iosProjectContent.replace(
     replaceRegex,
     `PRODUCT_NAME = ${this.projectName};\n\t\t\t\tTARGETED_DEVICE_FAMILY = "1,2";`
   );
-  this.fs.write(`${this.projectName}/ios/${this.projectName}.xcodeproj/project.pbxproj`, fixedProjectContent);
+  this.fs.write(
+    `${this.projectName}/ios/${this.projectName}.xcodeproj/project.pbxproj`,
+    fixedProjectContent
+  );
+  createSchemes.bind(this)();
 };
---
+++ generators/app/tasks/appSetup/multipleEnvIos/createSchemes.js
@@ -0,0 +1,10 @@
+const schemeBase = require("./schemeBase");
+
+module.exports = function createSchemes() {
+  const commonPath = schemeName =>
+    `${this.projectName}/ios/${this.projectName}.xcodeproj/xcshareddata/xcschemes/${schemeName}.xcscheme`;
+  this.fs.write(commonPath("qa"), schemeBase.bind(this)("dev"));
+  this.fs.write(commonPath("dev"), schemeBase.bind(this)("dev"));
+  this.fs.write(commonPath("stage"), schemeBase.bind(this)("stage"));
+  this.fs.write(commonPath("production"), schemeBase.bind(this)("production"));
+};
