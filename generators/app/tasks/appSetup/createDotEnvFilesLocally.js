b1ed36cef199fdc59a1a97440af49d64ced81d0b
Felipe Rodriguez Esturo <felipe.rodriguez@wolox.com.ar>
2020-04-01 16:09:51 -0300

    remove console.log

--- generators/app/tasks/appSetup/createDotEnvFilesLocally.js
+++ generators/app/tasks/appSetup/createDotEnvFilesLocally.js
@@ -1,5 +1,4 @@
 module.exports = function createDotEnvFilesLocally() {
-  console.log(this.projectName);
   this.fs.write(`${this.projectName}/.env.dev`, 'EMPTY_VARIABLE=DEVELOPMENT');
   this.fs.write(`${this.projectName}/.env.stage`, 'EMPTY_VARIABLE=STAGE');
   this.fs.write(`${this.projectName}/.env.production`, 'EMPTY_VARIABLE=PRODUCTION');
