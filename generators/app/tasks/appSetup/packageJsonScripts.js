4d5ea942ab15bde3c2cf35a552a554f07025a51b
Felipe Rodriguez Esturo <felipe.rodriguez@wolox.com.ar>
2020-04-02 14:58:24 -0300

    remove normal build script

--- generators/app/tasks/appSetup/packageJsonScripts.js
+++ generators/app/tasks/appSetup/packageJsonScripts.js
@@ -6,7 +6,6 @@ module.exports = function packgeJsonScripts() {
   packageJson.scripts['force-clean'] =
     'npm run android:clean && rm -rf $TMPDIR/react-* && watchman watch-del-all && rm -rf ios/build && rm -rf node_modules/ && npm cache clean && npm i';
   packageJson.scripts['android:clean'] = 'cd android/ && ./gradlew clean';
-  packageJson.scripts['android:build'] = 'cd android && ./gradlew clean && ./gradlew assembleRelease';
   packageJson.scripts['android:install'] =
     'cd android && ./gradlew clean && ./gradlew assembleRelease && ./gradlew installRelease';
 
