030241fb53666f656ae64c66cdf352a4e7935498
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2020-04-07 19:18:35 -0300

    All ready and fixed in the generator and update_app_identifier

--- generators/app/tasks/configureFastlane.js
+++ generators/app/tasks/configureFastlane.js
@@ -38,5 +38,25 @@ module.exports = function configureFastlane() {
         successMessage: 'Delete Success!',
         context: this.options
       })
-    );
+    )
+    .then(() => {
+      const fastlaneEnv = this.fs.read(`${this.projectName}/ios/fastlane/config/.env`);
+      const updateFastlaneEnv = fastlaneEnv.concat(
+        `PROJECT_FILE_NAME="${this.projectName}"\nXCODE_PROJECT_PATH="${process.cwd()}/${
+          this.projectName
+        }/ios/${this.projectName}.xcodeproj"`
+      );
+      this.fs.write(`${this.projectName}/ios/fastlane/config/.env`, updateFastlaneEnv);
+
+      const fastlaneAction = this.fs.read(`${this.projectName}/ios/fastlane/actions/project_name.rb`);
+      let updateFastlaneAction = fastlaneAction.replace(
+        'env_info[PROJECT_NAME_ENV_KEY] + env_info[PROJECT_EXTENSION_ENV_KEY] || DEFAULT_PROJECT_EXTENSION',
+        `env_info[XCODE_PROJECT_PWD] || (ENV['PWD'] + "/" + env_info[PROJECT_NAME_ENV_KEY] + (env_info[PROJECT_EXTENSION_ENV_KEY] || DEFAULT_PROJECT_EXTENSION))`
+      );
+      updateFastlaneAction = updateFastlaneAction.replace(
+        'PROJECT_NAME_ENV_KEY = "PROJECT_FILE_NAME".freeze\n',
+        'PROJECT_NAME_ENV_KEY = "PROJECT_FILE_NAME".freeze\n\t\t\tXCODE_PROJECT_PWD = "XCODE_PROJECT_PATH".freeze'
+      );
+      this.fs.write(`${this.projectName}/ios/fastlane/actions/project_name.rb`, updateFastlaneAction);
+    });
 };
