318d77a4556238b03d4ff85afd128b1d2b29ce99
Ignacio Santise <ignacio.santise@wolox.com.r>
2020-04-16 11:07:12 -0300

    Copying ios and android gemfiles, removed ios template gemfile

--- generators/app/tasks/addFilesToGitIgnore.js
+++ generators/app/tasks/addFilesToGitIgnore.js
@@ -2,8 +2,6 @@ module.exports = function addFilesToGitIgnore() {
   const gitIgnoreFile = this.fs.read(`${this.projectName}/.gitignore`);
   let updatedGitIgnoreFile = gitIgnoreFile.concat('\n# History (vs code)\n.history\n');
   updatedGitIgnoreFile = updatedGitIgnoreFile.concat('\n# Env\n/*.env\n');
-  updatedGitIgnoreFile = updatedGitIgnoreFile.concat('\n# Mac noise\n.DS_Store\n');
-  updatedGitIgnoreFile = updatedGitIgnoreFile.concat('\n# Fastlane noise\n*/report.xml\n');
   if (
     this.features.crashlytics ||
     this.features.firebaseanalytics ||
--- generators/app/tasks/configureFastlane.js
+++ generators/app/tasks/configureFastlane.js
@@ -15,26 +15,38 @@ function configureIosFastlane() {
     ],
     successMessage: 'Moved ios lanes!',
     context: this.options
-  }).then(() => {
-    const fastlaneEnv = this.fs.read(`${this.projectName}/ios/fastlane/config/.env`);
-    const updateFastlaneEnv = fastlaneEnv.concat(
-      `PROJECT_FILE_NAME="${this.projectName}"\nXCODE_PROJECT_PATH="${process.cwd()}/${
-        this.projectName
-      }/ios/${this.projectName}.xcodeproj"`
-    );
-    this.fs.write(`${this.projectName}/ios/fastlane/config/.env`, updateFastlaneEnv);
+  })
+    .then(() => {
+      runCommand({
+        command: [
+          'mv',
+          [`${fastlaneMobileRoot}/ios/Gemfile`, `ios`],
+          { cwd: `${process.cwd()}/${this.projectName}` }
+        ],
+        successMessage: 'Moved Gemfile!',
+        context: this.options
+      });
+    })
+    .then(() => {
+      const fastlaneEnv = this.fs.read(`${this.projectName}/ios/fastlane/config/.env`);
+      const updateFastlaneEnv = fastlaneEnv.concat(
+        `PROJECT_FILE_NAME="${this.projectName}"\nXCODE_PROJECT_PATH="${process.cwd()}/${
+          this.projectName
+        }/ios/${this.projectName}.xcodeproj"`
+      );
+      this.fs.write(`${this.projectName}/ios/fastlane/config/.env`, updateFastlaneEnv);
 
-    const fastlaneAction = this.fs.read(`${this.projectName}/ios/fastlane/actions/project_name.rb`);
-    let updateFastlaneAction = fastlaneAction.replace(
-      `ENV['PWD'] + "/" + env_info[PROJECT_NAME_ENV_KEY] + (env_info[PROJECT_EXTENSION_ENV_KEY] || DEFAULT_PROJECT_EXTENSION)`,
-      `env_info[XCODE_PROJECT_PWD] || ENV['PWD'] + "/" + env_info[PROJECT_NAME_ENV_KEY] + (env_info[PROJECT_EXTENSION_ENV_KEY] || DEFAULT_PROJECT_EXTENSION)`
-    );
-    updateFastlaneAction = updateFastlaneAction.replace(
-      'PROJECT_NAME_ENV_KEY = "PROJECT_FILE_NAME".freeze\n',
-      'PROJECT_NAME_ENV_KEY = "PROJECT_FILE_NAME".freeze\n\t\t\tXCODE_PROJECT_PWD = "XCODE_PROJECT_PATH".freeze'
-    );
-    this.fs.write(`${this.projectName}/ios/fastlane/actions/project_name.rb`, updateFastlaneAction);
-  });
+      const fastlaneAction = this.fs.read(`${this.projectName}/ios/fastlane/actions/project_name.rb`);
+      let updateFastlaneAction = fastlaneAction.replace(
+        `ENV['PWD'] + "/" + env_info[PROJECT_NAME_ENV_KEY] + (env_info[PROJECT_EXTENSION_ENV_KEY] || DEFAULT_PROJECT_EXTENSION)`,
+        `env_info[XCODE_PROJECT_PWD] || ENV['PWD'] + "/" + env_info[PROJECT_NAME_ENV_KEY] + (env_info[PROJECT_EXTENSION_ENV_KEY] || DEFAULT_PROJECT_EXTENSION)`
+      );
+      updateFastlaneAction = updateFastlaneAction.replace(
+        'PROJECT_NAME_ENV_KEY = "PROJECT_FILE_NAME".freeze\n',
+        'PROJECT_NAME_ENV_KEY = "PROJECT_FILE_NAME".freeze\n\t\t\tXCODE_PROJECT_PWD = "XCODE_PROJECT_PATH".freeze'
+      );
+      this.fs.write(`${this.projectName}/ios/fastlane/actions/project_name.rb`, updateFastlaneAction);
+    });
 }
 
 function configureAndroidFastlane() {
@@ -55,6 +67,17 @@ function configureAndroidFastlane() {
           { cwd: `${process.cwd()}/${this.projectName}` }
         ],
         successMessage: 'Moved version.gradle!',
+        context: this.options
+      });
+    })
+    .then(() => {
+      runCommand({
+        command: [
+          'mv',
+          [`${fastlaneMobileRoot}/android/Gemfile`, `android`],
+          { cwd: `${process.cwd()}/${this.projectName}` }
+        ],
+        successMessage: 'Moved Gemfile!',
         context: this.options
       });
     })
