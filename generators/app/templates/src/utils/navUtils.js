9874de298bdde7ff0cfae45d099107c44ff5602e
Felipe Rodriguez Esturo <felipe.rodriguez@wolox.com.ar>
2020-04-27 13:03:33 -0300

    add utils

--- generators/app/templates/src/utils/navUtils.js
+++ generators/app/templates/src/utils/navUtils.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { appScreensNavOptions } from '@config/navigation';
 import Routes from '@constants/routes';
 
+export function getActiveRoute(state) {
+  const route = state.routes[state.index];
+
+  if (route.state) {
+    return getActiveRoute(route.state);
+  }
+
+  return route;
+}
+
 export function inferRoute(NavigationStructure) {
   return function screenComponent(screenObj) {
     const screenName = Routes[Object.keys(screenObj)?.[0]];
