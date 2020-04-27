f5ab8b5c678bd3b1c12a163d2a6979916f970ddf
Felipe Rodriguez Esturo <felipe.rodriguez@wolox.com.ar>
2020-04-27 10:56:49 -0300

    get activeRoute navigation function

--- generators/app/templates/src/app/components/Navigation/index.js
+++ generators/app/templates/src/app/components/Navigation/index.js
@@ -5,3 +5,17 @@ export const navigationRef = React.createRef();
 export default function useNavigation() {
   return navigationRef.current;
 }
+
+function getRoute(state) {
+  const route = state.routes[state.index];
+
+  if (route.state) {
+    return getRoute(route.state);
+  }
+
+  return route;
+}
+
+export function getActiveRoute() {
+  return getRoute(navigationRef.current.getRootState());
+}
