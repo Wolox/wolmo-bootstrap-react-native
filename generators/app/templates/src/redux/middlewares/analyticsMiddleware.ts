e9ac6cb9792aa64ae9771dc10e26864a28a000d5
Felipe Rodriguez Esturo <felipe.rodriguez@wolox.com.ar>
2020-04-30 18:00:40 -0300

    another fix

--- generators/app/templates/src/redux/middlewares/analyticsMiddleware.ts
+++ generators/app/templates/src/redux/middlewares/analyticsMiddleware.ts
@@ -1,15 +1,15 @@
 import { Dispatch } from 'react';
-import { NavigationActions, StackActions } from 'react-navigation';
+import { NavigationActions, StackActions, NavigationAction } from 'react-navigation';
 import analytics from '@react-native-firebase/analytics';
-import { Action, ReduxObject } from '@interfaces/reduxInterfaces';
+import { ReduxObject } from '@interfaces/reduxInterfaces';
 import { getPreviousRouteName } from '@utils/navUtils';
 
-const trackNavigation = (routeName: string) => {
+const trackNavigation = (routeName: string | null) => {
   if (routeName) analytics().setCurrentScreen(routeName, `${routeName}.js`);
 };
 
 const eventsTrackingMiddleware = ({ getState }: ReduxObject) => (next: Dispatch<any>) => (
-  action: Action<any>
+  action: NavigationAction
 ) => {
   switch (action.type) {
     case NavigationActions.NAVIGATE:
