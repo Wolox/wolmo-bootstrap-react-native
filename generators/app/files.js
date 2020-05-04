156935a8e0c9bfb1a0b18df4338d8b75bad6a3ee
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2020-05-04 11:45:51 -0300

    Finished Linter and TS fixes. All ready

--- generators/app/files.js
+++ generators/app/files.js
@@ -78,7 +78,7 @@ module.exports.API_CONFIG = `${module.exports.CONFIG_PATH}/api.ts`;
 module.exports.CONFIG = `${module.exports.CONFIG_PATH}/index.ts`;
 module.exports.REACTOTRON_CONFIG = `${module.exports.CONFIG_PATH}/reactotronConfig.ts`;
 module.exports.I18N_CONFIG = `${module.exports.CONFIG_PATH}/i18n.ts`;
-module.exports.NAVIGATION_CONFIG = `${module.exports.CONFIG_PATH}/navigation.ts`;
+module.exports.NAVIGATION_CONFIG = `${module.exports.CONFIG_PATH}/navigation.tsx`;
 module.exports.FONTS_CONFIG = `${module.exports.CONFIG_PATH}/fonts.ts`;
 
 // CONSTANTS
