6ee1010f09c1257f0be9752bdf3b646d1af60a20
Felipe Rodriguez Esturo <felipe.rodriguez@wolox.com.ar>
2020-05-25 17:25:15 -0300

    merge interface

--- generators/app/tasks/appSetup/loginFeatureFiles.js
+++ generators/app/tasks/appSetup/loginFeatureFiles.js
@@ -1,7 +1,13 @@
 const { copyFile } = require('../../utils');
-const { LOGIN_PATH, TESTS_AUTH_PATH, TESTS_LOGIN_SCREEN_PATH, TESTS_UTILS } = require('../../files');
+const {
+  LOGIN_PATH,
+  TESTS_AUTH_PATH,
+  TESTS_LOGIN_SCREEN_PATH,
+  TESTS_UTILS,
+  AUTH_INTERFACES
+} = require('../../files');
 
-const FILES = [LOGIN_PATH, TESTS_AUTH_PATH, TESTS_LOGIN_SCREEN_PATH, TESTS_UTILS];
+const FILES = [LOGIN_PATH, TESTS_AUTH_PATH, TESTS_LOGIN_SCREEN_PATH, TESTS_UTILS, AUTH_INTERFACES];
 
 module.exports = function loginFeatureFiles() {
   FILES.forEach(copyFile.bind(this));
