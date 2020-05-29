ae33ecbeacf84527099cbcb7e0a6d19a6418eaaf
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2020-05-29 17:14:57 -0300

    Fix error with  double ## in colors

--- generators/app/templates/src/constants/colors.ts
+++ generators/app/templates/src/constants/colors.ts
@@ -5,4 +5,4 @@ export const transparent: string = 'transparent';
 export const white: string = '#FFF';
 export const black: string = '#000';
 export const gray: string = '#rgba(0, 0, 0, 0.38)';
-export const darkGray: string = '##363636';
+export const darkGray: string = '#363636';
