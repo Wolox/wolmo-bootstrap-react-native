8806ca89850811f3fae0261fdd52796b67e58b83
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2020-05-31 21:23:19 -0300

    Final changes

--- generators/app/files.js
+++ generators/app/files.js
@@ -52,14 +52,11 @@ module.exports.APP_NAVIGATOR_PATH = `${module.exports.COMPONENTS_PATH}/AppNaviga
 module.exports.APP_NAVIGATOR = `${module.exports.APP_NAVIGATOR_PATH}/index.tsx`;
 module.exports.APP_NAVIGATOR_NAVIGATOR = `${module.exports.APP_NAVIGATOR_PATH}/navigator.tsx`;
 module.exports.NAVIGATION_HELPER = `${module.exports.APP_NAVIGATOR_PATH}/helper.ts`;
-
-// COMPONENTS - CUSTOM STATUS BAR
-module.exports.CUSTOM_STATUS_BAR = `${module.exports.COMPONENTS_PATH}/CustomStatusBar`;
-
 // APP COMPONENTS - CUSTOM BUTTON
 module.exports.CUSTOM_BUTTON_PATH = `${module.exports.COMPONENTS_PATH}/CustomButton`;
-
-// COMPONENTS - CUSTOM TEXT
+// APP COMPONENTS - CUSTOM STATUS BAR
+module.exports.CUSTOM_STATUS_BAR = `${module.exports.COMPONENTS_PATH}/CustomStatusBar`;
+// APP COMPONENTS - CUSTOM TEXT
 module.exports.CUSTOM_TEXT_PATH = `${module.exports.COMPONENTS_PATH}/CustomText`;
 // APP COMPONENTS - CUSTOM TEXT INPUT
 module.exports.CUSTOM_TEXT_INPUT_PATH = `${module.exports.COMPONENTS_PATH}/CustomTextInput`;
@@ -74,7 +71,6 @@ module.exports.HOME = `${module.exports.HOME_PATH}/index.tsx`;
 module.exports.HOME_STYLES = `${module.exports.HOME_PATH}/styles.ts`;
 // APP SCREENS - LOGIN
 module.exports.LOGIN_PATH = `${module.exports.SCREENS_PATH}/Login`;
-
 // APP SCREENS - ONBOARDING
 module.exports.ONBOARDING_PATH = `${module.exports.SCREENS_PATH}/OnBoarding`;
 
--- generators/app/tasks/appSetup/loginFeatureFiles.js
+++ generators/app/tasks/appSetup/loginFeatureFiles.js
@@ -1,21 +1,13 @@
 const { copyFile } = require('../../utils');
 const {
+  AUTH_INTERFACES,
   LOGIN_PATH,
   TESTS_AUTH_PATH,
   TESTS_LOGIN_SCREEN_PATH,
-  TESTS_UTILS,
-  AUTH_INTERFACES,
-  WITH_FORM_PATH
+  TESTS_UTILS
 } = require('../../files');
 
-const FILES = [
-  LOGIN_PATH,
-  TESTS_AUTH_PATH,
-  TESTS_LOGIN_SCREEN_PATH,
-  TESTS_UTILS,
-  AUTH_INTERFACES,
-  WITH_FORM_PATH
-];
+const FILES = [AUTH_INTERFACES, LOGIN_PATH, TESTS_AUTH_PATH, TESTS_LOGIN_SCREEN_PATH, TESTS_UTILS];
 
 module.exports = function loginFeatureFiles() {
   FILES.forEach(copyFile.bind(this));
