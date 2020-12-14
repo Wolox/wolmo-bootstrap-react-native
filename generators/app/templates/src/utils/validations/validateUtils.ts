f1fbe5d9f171b8022fb531a9afa9b2d7028f220b
Mariano Zicavo <marianozicavo@gmail.com>
2020-12-14 17:51:59 -0300

    Fix validation

--- generators/app/templates/src/utils/validations/validateUtils.ts
+++ generators/app/templates/src/utils/validations/validateUtils.ts
@@ -11,28 +11,28 @@ const onlyNumberRegex = /^[0-9]*$/g;
 // VALIDATIONS
 export const validateRequired = { required: i18next.t('VALIDATIONS:REQUIRED_FIELD') };
 
-export const validateEmail = { pattern: emailRegex, message: i18next.t('VALIDATIONS:INVALID_EMAIL') };
-
-export const validateAlphanumeric = {
-  pattern: alphanumericRegex,
-  message: i18next.t('VALIDATIONS:ALPHANUMERIC')
+export const validateEmail = {
+  pattern: { value: emailRegex, message: i18next.t('VALIDATIONS:INVALID_EMAIL') }
 };
 
-export const validateOnlyText = { pattern: onlyTextRegex, message: i18next.t('VALIDATIONS:ONLY_TEXT') };
+export const validateAlphanumeric = {
+  pattern: { value: alphanumericRegex, message: i18next.t('VALIDATIONS:ALPHANUMERIC') }
+};
+
+export const validateOnlyText = {
+  pattern: { value: onlyTextRegex, message: i18next.t('VALIDATIONS:ONLY_TEXT') }
+};
 
 export const validateOnlyNumber = {
-  pattern: onlyNumberRegex,
-  message: i18next.t('VALIDATIONS:ONLY_NUMBERS')
+  pattern: { value: onlyNumberRegex, message: i18next.t('VALIDATIONS:ONLY_NUMBERS') }
 };
 
 export const validateMinLength = (minValue: number) => ({
-  minLength: minValue,
-  message: i18next.t('VALIDATIONS:MIN_LENGTH', { count: minValue })
+  minLength: { value: minValue, message: i18next.t('VALIDATIONS:MIN_LENGTH', { count: minValue }) }
 });
 
 export const validateMaxLength = (maxValue: number) => ({
-  maxLength: maxValue,
-  message: i18next.t('VALIDATIONS:MAX_LENGTH', { count: maxValue })
+  maxLength: { value: maxValue, message: i18next.t('VALIDATIONS:MAX_LENGTH', { count: maxValue }) }
 });
 
 export const validateEqualLength = (equalValue: number) => ({
