5e4d645c2b88d12c7c9bddf1d0d6098db5760261
Mariano Zicavo <marianozicavo@gmail.com>
2020-12-15 09:44:39 -0300

    fix input warning

--- generators/app/templates/src/app/components/CustomTextInput/controller.tsx
+++ generators/app/templates/src/app/components/CustomTextInput/controller.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
+import { TextInput } from 'react-native';
 import { Controller, ControllerProps } from 'react-hook-form';
 import CustomTextInput from '@components/CustomTextInput';
 import { CustomTextInputProps } from '@components/CustomTextInput/interface';
 
 type Props = CustomTextInputProps & Omit<ControllerProps<typeof CustomTextInput>, 'render'>;
 
-function CustomTextInputController(props: Props) {
-  return <Controller as={CustomTextInput} defaultValue="" {...props} />;
-}
+const CustomTextInputController = forwardRef<TextInput, Props>((props, ref) => (
+  <Controller as={<CustomTextInput ref={ref} />} defaultValue="" {...props} />
+));
 
 export default CustomTextInputController;
--- generators/app/templates/src/app/components/CustomTextInput/index.tsx
+++ generators/app/templates/src/app/components/CustomTextInput/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, memo } from 'react';
+import React, { useState, memo, forwardRef } from 'react';
 import { TextInput, View } from 'react-native';
 import CustomText from '@components/CustomText';
 import { transparent } from '@constants/colors';
@@ -8,83 +8,89 @@ import ShowPassword from './components/ShowPassword';
 import { CustomTextInputProps as Props } from './interface';
 import styles from './styles';
 
-const CustomTextInput = ({
-  animated,
-  autoCompleteType,
-  disabled,
-  error,
-  errorContainerStyle,
-  errorStyle,
-  inputContainerStyle,
-  inputTextStyles,
-  isFocused,
-  isOptional,
-  label,
-  labelStyle,
-  multiline,
-  onChange,
-  placeholder,
-  placeholderColor,
-  secureTextEntry,
-  showError,
-  showEye,
-  style,
-  value,
-  ...props
-}: Props) => {
-  const [showPassword, setShowPassword] = useState(false);
-  const handleShowPassword = () => setShowPassword(prevShowPassword => !prevShowPassword);
-  const borderColorStyle = () => {
-    if (disabled) return styles.bottomBorderLightGray;
-    if (isFocused) return styles.bottomBorderBlue;
-    if (showError) return styles.bottomBorderRed;
-    return {};
-  };
-  return (
-    <View style={[styles.container, animated && !!label && styles.withAnimatedLabel, style]}>
-      {label && (
-        <InputLabel
-          animated={animated}
-          hasValue={!!value}
-          isFocused={isFocused}
-          isOptional={isOptional}
-          label={label}
-          labelStyle={labelStyle}
-        />
-      )}
-      <View
-        style={[
-          multiline ? styles.multilineContainer : styles.inputContainer,
-          borderColorStyle(),
-          inputContainerStyle
-        ]}>
-        <TextInput
-          {...props}
-          autoCompleteType={secureTextEntry ? 'off' : autoCompleteType}
-          editable={!disabled}
-          multiline={multiline}
-          onChangeText={onChange}
-          placeholder={(isFocused || !animated) && value === '' ? placeholder : ''}
-          placeholderTextColor={placeholderColor}
-          secureTextEntry={secureTextEntry && !showPassword}
-          style={[styles.inputStyle, !multiline && styles.singleInput, inputTextStyles]}
-          value={value}
-          testID={label || placeholder}
-        />
-        {secureTextEntry && showEye && (
-          <ShowPassword onShowPassword={handleShowPassword} passwordVisible={showPassword} />
+const CustomTextInput = forwardRef<TextInput, Props>(
+  (
+    {
+      animated,
+      autoCompleteType,
+      disabled,
+      error,
+      errorContainerStyle,
+      errorStyle,
+      inputContainerStyle,
+      inputTextStyles,
+      isFocused,
+      isOptional,
+      label,
+      labelStyle,
+      multiline,
+      onChange,
+      placeholder,
+      placeholderColor,
+      secureTextEntry,
+      showError,
+      showEye,
+      style,
+      value,
+      ...props
+    },
+    ref
+  ) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const handleShowPassword = () => setShowPassword(prevShowPassword => !prevShowPassword);
+    const borderColorStyle = () => {
+      if (disabled) return styles.bottomBorderLightGray;
+      if (isFocused) return styles.bottomBorderBlue;
+      if (showError) return styles.bottomBorderRed;
+      return {};
+    };
+    return (
+      <View style={[styles.container, animated && !!label && styles.withAnimatedLabel, style]}>
+        {label && (
+          <InputLabel
+            animated={animated}
+            hasValue={!!value}
+            isFocused={isFocused}
+            isOptional={isOptional}
+            label={label}
+            labelStyle={labelStyle}
+          />
         )}
+        <View
+          style={[
+            multiline ? styles.multilineContainer : styles.inputContainer,
+            borderColorStyle(),
+            inputContainerStyle
+          ]}>
+          <TextInput
+            {...props}
+            ref={ref}
+            autoCompleteType={secureTextEntry ? 'off' : autoCompleteType}
+            editable={!disabled}
+            multiline={multiline}
+            onChangeText={onChange}
+            placeholder={(isFocused || !animated) && value === '' ? placeholder : ''}
+            placeholderTextColor={placeholderColor}
+            secureTextEntry={secureTextEntry && !showPassword}
+            style={[styles.inputStyle, !multiline && styles.singleInput, inputTextStyles]}
+            value={value}
+            testID={label || placeholder}
+          />
+          {secureTextEntry && showEye && (
+            <ShowPassword onShowPassword={handleShowPassword} passwordVisible={showPassword} />
+          )}
+        </View>
+        <View style={[!disabled && styles.errorContainer, errorContainerStyle]}>
+          {!isFocused && error && (
+            <CustomText error xsmall style={errorStyle}>
+              {error}
+            </CustomText>
+          )}
+        </View>
       </View>
-      <View style={[!disabled && styles.errorContainer, errorContainerStyle]}>
-        {!isFocused && error && (
-          <CustomText error xsmall style={errorStyle}>
-            {error}
-          </CustomText>
-        )}
-      </View>
-    </View>
-  );
-};
+    );
+  }
+);
 
 CustomTextInput.defaultProps = {
   allowFontScaling: false,
@@ -95,7 +101,6 @@ CustomTextInput.defaultProps = {
   clearButtonMode: 'never',
   disabled: false,
   keyboardType: 'default',
-  maxHeight: 200,
   multiline: false,
   placeholder: '',
   returnKeyType: 'done',
