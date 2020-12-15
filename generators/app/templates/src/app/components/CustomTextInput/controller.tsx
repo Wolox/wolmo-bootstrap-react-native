af972a93a192c9c19c3553706984e86e123a067e
Mariano Zicavo <marianozicavo@gmail.com>
2020-12-15 11:32:51 -0300

    Fix input ref

--- generators/app/templates/src/app/components/CustomTextInput/controller.tsx
+++ generators/app/templates/src/app/components/CustomTextInput/controller.tsx
@@ -4,10 +4,36 @@ import { Controller, ControllerProps } from 'react-hook-form';
 import CustomTextInput from '@components/CustomTextInput';
 import { CustomTextInputProps } from '@components/CustomTextInput/interface';
 
-type Props = CustomTextInputProps & Omit<ControllerProps<typeof CustomTextInput>, 'render'>;
+type Props = CustomTextInputProps & ControllerProps<typeof CustomTextInput>;
 
-const CustomTextInputController = forwardRef<TextInput, Props>((props, ref) => (
-  <Controller as={<CustomTextInput ref={ref} />} defaultValue="" {...props} />
-));
+const CustomTextInputController = forwardRef<TextInput, Props>(function CustomTextInputController(
+  { name, control, defaultValue = '', rules, onFocus, ...props },
+  ref
+) {
+  return (
+    <Controller
+      render={({ onChange, onBlur, ...renderProps }) => (
+        <CustomTextInput
+          {...props}
+          onChange={e => {
+            onChange(e);
+            if (props.onChange) props.onChange(e);
+          }}
+          onBlur={e => {
+            onBlur();
+            if (props.onBlur) props.onBlur(e);
+          }}
+          {...renderProps}
+          ref={ref}
+        />
+      )}
+      defaultValue={defaultValue}
+      name={name}
+      control={control}
+      rules={rules}
+      onFocus={onFocus}
+    />
+  );
+});
 
 export default CustomTextInputController;
