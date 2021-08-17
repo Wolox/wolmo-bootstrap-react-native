7a0d6e8f3ae37d639306017e89a71b6fad899c97
Santiago Fernandez <santiago.fernandez@wolox.com.ar>
2021-08-17 11:49:18 -0300

    Last changes in docs

--- generators/app/tasks/nextSteps.js
+++ generators/app/tasks/nextSteps.js
@@ -2,30 +2,33 @@ require('colors');
 
 module.exports = function nextSteps() {
   console.log(`\n       ${'NEXT STEPS!'.bold.underline.white}       \n`);
+
   // Introduction message
   console.log(
     `● Your project folder has been created and it already includes the kickoff commit. ${
       `cd ${this.projectName}/`.italic
     }`.cyan
   );
+
+  // Remote repository initialized
   if (this.repoUrl) {
-    // Remote repository initialized
     console.log(`● All this boilerplate has already been pushed to ${this.repoUrl}`.cyan);
   }
 
   // Firebase
-  if (this.features.firebasecrashlytics) {
+  if (this.features.hasFirebase) {
     console.log(
-      '  (*) Remember to ask your team lead for the google services file, this file must be replaced for the corresponding application of https://console.firebase.google.com'
+      '● Remember to ask your team lead for the google services files, these files must be replaced for the corresponding application of https://console.firebase.google.com'
         .cyan
     );
+    console.log(`● Remember to check the Firebase packages version in the Android's build.gradle file`.cyan);
   }
 
+  // Login and SignUp next steps
   if (this.features.loginandsignup) {
-    // Login and SignUp next steps
     console.log('● A basic login and signup feature has been added. The probably next steps there are:'.cyan);
-    console.log('  Add proper validations and styles to the login form'.cyan);
-    console.log('  Integrate login and logout with API'.cyan);
+    console.log('  Add proper validations and styles to the login and signup forms'.cyan);
+    console.log('  Integrate login, logout and signup with API'.cyan);
   }
 
   console.log('\n\n');
