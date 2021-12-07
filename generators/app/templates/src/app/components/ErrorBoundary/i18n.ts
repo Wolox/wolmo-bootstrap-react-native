51d0419081904d9db8f3b5b31e23c2afbe9dae94
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-12-07 11:40:54 -0300

    added errorBoundary component

---
+++ generators/app/templates/src/app/components/ErrorBoundary/i18n.ts
@@ -0,0 +1,13 @@
+import i18next from 'i18next';
+
+i18next.addResources('es', 'ERRORBOUNDARY', {
+  TITLE: '¡Oh no, a ocurrido un error!',
+  TRY_AGAIN: 'Intenta nuevamente',
+  BACK_TO_HOME: 'Volver al inicio'
+});
+
+i18next.addResources('en', 'ERRORBOUNDARY', {
+  TITLE: 'Oh no, an error occurred !',
+  TRY_AGAIN: 'Try again later',
+  BACK_TO_HOME: 'Back to home'
+});
---
+++ generators/app/templates/src/app/components/ErrorBoundary/index.tsx
@@ -0,0 +1,43 @@
+import React, { FunctionComponent, ReactElement } from 'react';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+
+import ErrorFallBack from './ErrorFallBack';
+
+/*
+in this function we can use or save the error on cloud with others libraries like Sentry
+import * as Sentry from '@sentry/react-native';
+*/
+const myErrorHandler = (error: Error) => {
+  // Example: Sentry.captureException(error);
+};
+
+// this function receive a children: ReactElement and return the children component or fallbackComponent, in this case <ErrroFallBack/> and if was an error onError was called
+export const ErrorHandler = ({
+  children,
+  component
+}: {
+  children: ReactElement;
+  component?: FunctionComponent<FallbackProps>;
+}) => {
+  console.log(typeof component);
+  return (
+    <ErrorBoundary
+      FallbackComponent={component || ErrorFallBack}
+      onError={myErrorHandler}>
+      {children}
+    </ErrorBoundary>
+  );
+};
+
+/*
+  how to test error:
+  tsx:
+  const [statusOk, setStatusOk] = useState(true);
+  useEffect(() => {
+    if (!statusOk) throw new Error('new error coming');
+  }, [statusOk]);
+  const handleError = useCallback(() => {
+    setStatusOk(false);
+  }, [Error]);
+return <Button onPress={handleError} title="get my error"/>
+ */
