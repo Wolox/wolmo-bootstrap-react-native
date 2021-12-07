28d83bcc36fbf822e0b34ae6110ee2787c80b8ec
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-12-07 18:03:13 -0300

    added i18next resourse expetion-translate

--- generators/app/templates/src/app/components/ErrorBoundary/i18n.ts
+++ generators/app/templates/src/app/components/ErrorBoundary/i18n.ts
@@ -3,5 +3,9 @@ import i18next from 'i18next';
 i18next.addResources('es', 'ERRORBOUNDARY', {
   TITLE: '¡Oh no, a ocurrido un error!',
   TRY_AGAIN: 'Intenta nuevamente',
-  BACK_TO_HOME: 'Volver al inicio'
+  BACK_TO_HOME: 'Volver al inicio',
+  ERROR_ALERT: 'Unexpected error occurred',
+  TYPE_ERROR: `Error: {{fatal}}`,
+  ERROR_MESSAGE:
+    '\nWe have reported this to our team ! Please close the app and start again!'
 });
